c49e4591eba860a62e5f1104fabbe07179948d9c
Manju166 <manjubhandariabc@gmail.com>
2024-08-08 17:01:24 +0545

    user authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_07_053812) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_08_065753) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "description"
     t.string "keywords"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_id"
   end
 
   create_table "users", force: :cascade do |t|
